375ff1c91308966b1b8d1d76e35f99194744599c
Dushyant Kumbhat <kumbhat10@gmail.com>
2022-02-14 01:00:55 +0000

    nvngvg

--- firebase_write.py
+++ firebase_write.py
@@ -9,34 +9,36 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 print("\n  Below is input arguments")
+input = sys.argv[1]
+print(input)
 
-print(sys.argv[1])
-data = json.loads(str(sys.argv[1])) 
+print("\n  Below is type of data")
+print(type(input))
+
+data = json.loads(input) 
 print("\n  Below is parsed JSON data")
 print(data)
 print("\n  Below is type of data")
 print(type(data))
 
-#time.sleep(5)
-cred = firebase_admin.credentials.Certificate(data )
+#cred = firebase_admin.credentials.Certificate(data )
 
-try:
-  firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://ttl-iot-default-rtdb.europe-west1.firebasedatabase.app'
-})  # Initialize the app with a service account, granting admin privileges
-except ValueError:
-  print('Firebase - Already initialized')
-except:
-  print('Firebase - Error occured')
-else:
-  print('Firebase - Initialized Successfully')
+# try:
+  # firebase_admin.initialize_app(cred, {
+    # 'databaseURL': 'https://ttl-iot-default-rtdb.europe-west1.firebasedatabase.app'
+# })  # Initialize the app with a service account, granting admin privileges
+# except ValueError:
+  # print('Firebase - Already initialized')
+# except:
+  # print('Firebase - Error occured')
+# else:
+  # print('Firebase - Initialized Successfully')
 
-ref = db.reference('CheckLive')
+# ref = db.reference('CheckLive')
 
-print("Writing to Firebase")
-now = datetime.now()
-current_time = now.strftime("%y-%m-%d %H:%M:%S")
-print("Current Time =", current_time)
-# ref.get()
-ref.set(current_time)
+# print("Writing to Firebase")
+# now = datetime.now()
+# current_time = now.strftime("%y-%m-%d %H:%M:%S")
+# print("Current Time =", current_time)
+# ref.set(current_time)
 
