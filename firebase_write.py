d12b564c2d6c1a820c4ec952d68b0c9acdd23d4d
Dushyant Kumbhat <kumbhat10@gmail.com>
2022-02-16 17:36:20 +0000

    Add Cloud notification option also

--- firebase_write.py
+++ firebase_write.py
@@ -3,18 +3,11 @@
 import os
 from datetime import datetime
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-
-cwd = os.getcwd()
-print(cwd)
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
+from firebase_admin import credentials, db, messaging
 
 #workspace = "D:" # for windows
 workspace = os.environ.get("GITHUB_WORKSPACE") #sys.argv[1]
 filename = 'Private-key.json'
-
 
 keypath = os.path.join(workspace, filename)
 cred = firebase_admin.credentials.Certificate(keypath)
@@ -24,17 +17,32 @@ try:
     'databaseURL': 'https://ttl-iot-default-rtdb.europe-west1.firebasedatabase.app'
 })  # Initialize the app with a service account, granting admin privileges
 except ValueError:
-  print('Firebase - Already initialized')
+  print('\nFirebase - Already initialized')
 except:
-  print('Firebase - Error occured')
+  print('\nFirebase - Error occured')
 else:
-  print('Firebase - Initialized Successfully')
+  print('\nFirebase - Initialized Successfully')
 
 ref = db.reference('CheckLive')
 
-print("Writing to Firebase")
+print("\nWriting to Firebase")
 now = datetime.now()
 current_time = now.strftime("%y-%m-%d %H:%M:%S")
-print("Current Time =", current_time)
+print("\nCurrent Time =", current_time)
 ref.set(current_time)
+
+
+def sendCloudNotification():
+    topic = 'Alert'
+    message = messaging.Message(
+    topic=topic, android=messaging.AndroidConfig(priority='high',
+                                        data = {'Topic':'332'},
+            notification=messaging.AndroidNotification(
+                title='Github Actions Update',
+                body='Github actions successfully finished at '+current_time,
+                image="https://i.pinimg.com/564x/92/fb/38/92fb38bd608b0647cbc7b33270f86e56.jpg")))
+    response = messaging.send(message) # Response is a message ID string.
+    print('\nSuccessfully sent cloud notification :', response)
+    
+
 
