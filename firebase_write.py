5e0f508fab2fec132c3c67e6f862aa188e8423b4
Dushyant Kumbhat <kumbhat10@gmail.com>
2022-02-16 17:23:32 +0000

    Update YAML for Ubuntu

--- firebase_write.py
+++ firebase_write.py
@@ -1,10 +1,7 @@
 
 #pip install --upgrade firebase-admin
 import os
-import sys
-import json
-from datetime import datetime
-import time
+import datetime
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
@@ -14,28 +11,30 @@ print(cwd)
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print(dir_path)
 
-#key = os.environ.get("FIREBASE_SA_JSON") #sys.argv[1]  #
+#workspace = "D:" # for windows
+workspace = os.environ.get("GITHUB_WORKSPACE") #sys.argv[1]
+filename = 'Private-key.json'
 
-keypath = "D:/Private-key.json"
 
-# cred = firebase_admin.credentials.Certificate(keypath )
+keypath = os.path.join(workspace, filename)
+cred = firebase_admin.credentials.Certificate(keypath)
 
-# try:
-  # firebase_admin.initialize_app(cred, {
-    # 'databaseURL': 'https://ttl-iot-default-rtdb.europe-west1.firebasedatabase.app'
-# })  # Initialize the app with a service account, granting admin privileges
-# except ValueError:
-  # print('Firebase - Already initialized')
-# except:
-  # print('Firebase - Error occured')
-# else:
-  # print('Firebase - Initialized Successfully')
+try:
+  firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://ttl-iot-default-rtdb.europe-west1.firebasedatabase.app'
+})  # Initialize the app with a service account, granting admin privileges
+except ValueError:
+  print('Firebase - Already initialized')
+except:
+  print('Firebase - Error occured')
+else:
+  print('Firebase - Initialized Successfully')
 
-# ref = db.reference('CheckLive')
+ref = db.reference('CheckLive')
 
-# print("Writing to Firebase")
-# now = datetime.now()
-# current_time = now.strftime("%y-%m-%d %H:%M:%S")
-# print("Current Time =", current_time)
-# ref.set(current_time)
+print("Writing to Firebase")
+now = datetime.now()
+current_time = now.strftime("%y-%m-%d %H:%M:%S")
+print("Current Time =", current_time)
+ref.set(current_time)
 
