fed9fe5859f28dd7e7d973d811e3830fd8f7dfbc
Dushyant Kumbhat <kumbhat10@gmail.com>
2022-02-16 17:40:30 +0000

    Define the function before calling it

--- firebase_write.py
+++ firebase_write.py
@@ -5,6 +5,19 @@ from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials, db, messaging
 
+## Function to send mobile cloud notification to all the users
+def sendCloudNotification():
+    topic = 'Alert'
+    message = messaging.Message(
+    topic=topic, android=messaging.AndroidConfig(priority='high',
+                                        data = {'Topic':'332'},
+            notification=messaging.AndroidNotification(
+                title='Github Actions Update',
+                body='Github actions successfully finished at '+current_time,
+                image="https://i.pinimg.com/564x/92/fb/38/92fb38bd608b0647cbc7b33270f86e56.jpg")))
+    response = messaging.send(message) # Response is a message ID string.
+    print('\nSuccessfully sent cloud notification :', response)
+    
 #workspace = "D:" # for windows
 workspace = os.environ.get("GITHUB_WORKSPACE") #sys.argv[1]
 filename = 'Private-key.json'
@@ -32,17 +45,7 @@ ref.set(current_time)
 sendCloudNotification()
 print("\nCurrent Time =", current_time)
 
-def sendCloudNotification():
-    topic = 'Alert'
-    message = messaging.Message(
-    topic=topic, android=messaging.AndroidConfig(priority='high',
-                                        data = {'Topic':'332'},
-            notification=messaging.AndroidNotification(
-                title='Github Actions Update',
-                body='Github actions successfully finished at '+current_time,
-                image="https://i.pinimg.com/564x/92/fb/38/92fb38bd608b0647cbc7b33270f86e56.jpg")))
-    response = messaging.send(message) # Response is a message ID string.
-    print('\nSuccessfully sent cloud notification :', response)
+
     
 
 
