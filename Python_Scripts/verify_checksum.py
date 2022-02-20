2bba682c009548c8f5f92127d26734bf6963f85b
Dushyant Kumbhat <kumbhat10@gmail.com>
2022-02-20 15:12:31 +0000

    Ec

--- Python_Scripts/verify_checksum.py
+++ Python_Scripts/verify_checksum.py
@@ -8,7 +8,8 @@ from dateutil import parser
 
 workspace = os.environ.get("GITHUB_WORKSPACE")
 event_context = os.environ.get("EVENT_CONTEXT")
-
+print(type(event_context))
+ec = json.loads(event_context)
 filename = 'Python_Scripts/Private-key.json'
 keypath = os.path.join(workspace, filename)
 cred = firebase_admin.credentials.Certificate(keypath)
@@ -16,6 +17,8 @@ raw_bin_file_path = 'Excavator/build/esp32.esp32.esp32/Excavator.ino.bin'
 raw_bin_file_path = os.path.join(workspace, raw_bin_file_path)
 commit_timestamp = parser.parse(os.environ.get("COMMIT_TIMESTAMP")).strftime("%Y%m%d_%H%M%S")
 
+commit_time = parser.parse(os.environ.get("COMMIT_TIMESTAMP")).strftime("%H%M%S")
+commit_date = parser.parse(os.environ.get("COMMIT_TIMESTAMP")).strftime("%Y%m%d")
 def file_as_bytes(file):
     with file:
         return file.read()
@@ -38,7 +41,7 @@ firebase_login()
 ## get last firmware Version
 f_db = firestore.client()
 doc_ref_firmware = f_db.collection('Firmware')
-doc_ref = doc_ref_firmware.document('destination_blob_name').set(event_context)
+doc_ref = doc_ref_firmware.document('destination_blob_name').set(ec)
 
 
 destination_blob_name = 'Firmware_' + commit_timestamp + '_10.bin'
