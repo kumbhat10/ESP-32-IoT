34700f3f064abe7ce71b8fab5467b919101e07bb
Dushyant Kumbhat <kumbhat10@gmail.com>
2022-02-20 15:23:32 +0000

    Checksum done

--- Python_Scripts/verify_checksum.py
+++ Python_Scripts/verify_checksum.py
@@ -7,9 +7,9 @@ from google.cloud import storage
 from dateutil import parser
 
 workspace = os.environ.get("GITHUB_WORKSPACE")
-event_context = os.environ.get("EVENT_CONTEXT")
-print(type(event_context))
-ec = json.loads(event_context)
+event_context_string = os.environ.get("EVENT_CONTEXT")
+
+event_context_json = json.loads(event_context_string)
 filename = 'Python_Scripts/Private-key.json'
 keypath = os.path.join(workspace, filename)
 cred = firebase_admin.credentials.Certificate(keypath)
@@ -24,7 +24,7 @@ def file_as_bytes(file):
         return file.read()
 def checksum(bin_file_path):
     return hashlib.md5(file_as_bytes(open(bin_file_path, 'rb'))).hexdigest()
-# checksum = checksum(bin_file_path)
+checksum = 'w3r3wc3rc443r34rc344r344c' #checksum(bin_file_path)
 def firebase_login():
     try:
       firebase_admin.initialize_app(cred, {
@@ -41,10 +41,25 @@ firebase_login()
 ## get last firmware Version
 f_db = firestore.client()
 doc_ref_firmware = f_db.collection('Firmware')
-doc_ref = doc_ref_firmware.document('destination_blob_name').set(ec)
+query = doc_ref_firmware.order_by("_firmware_version",  direction=firestore.Query.DESCENDING).limit(1)
+results = query.stream()
+for doc in results:
+  document = doc.to_dict()
+  last_firmware_version = document['_firmware_version']
+  last_firmware_checksum = document['_firmware_checksum']
+print("last_firmware_version :", last_firmware_version)
+print("last_firmware_checksum :", last_firmware_checksum)
+
+current_firmware_version = int(last_firmware_version) + 1;
+current_firmware_name = 'Firmware_' + commit_timestamp + '_' + str(version)
 
 
-destination_blob_name = 'Firmware_' + commit_timestamp + '_10.bin'
+event_context_json['_firmware_version'] = current_firmware_version
+event_context_json['_firmware_checksum'] = checksum
+event_context_json['_firmware_date'] = int(commit_date)
+event_context_json['_firmware_time'] = int(commit_time)
+doc_ref = doc_ref_firmware.document(current_firmware_name).set(event_context_json)
+
 # filepath = "C:/Users/kumbh/Documents/Arduino/GIT Repo/ESP-32-IoT-Excavator/Excavator/firmware_20220219_134302_6.bin"
 
 # print("   ")
